1cdf1fbb711811e348999613dd8c9d90f1ad0bbf
Keiji Suzuki <zuki.ebetsu@gmail.com>
2019-06-26 16:36:12 +0900

    ver 0.1

--- RaspiOperator/ViewController.swift
+++ RaspiOperator/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CocoaMQTT
 
 class ViewController: UIViewController {
-
+      let mqttClient = CocoaMQTT(clientID: "iOS Device", host: "192.168.11.12", port: 1883)
+    @IBOutlet weak var connBtn: UIButton!
+    @IBOutlet weak var disconnBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.connBtn.setTitleColor(UIColor.darkText, for: .normal)
+        self.disconnBtn.setTitleColor(UIColor.darkText, for: .normal)
+    }
+
+    @IBAction func gpio40SW(_ sender: UISwitch) {
+        // If switch is ON, publish to topi "rpi/gpio" with message "on", else publish message "off"
+        if sender.isOn {
+            mqttClient.publish("rpi/gpio", withString: "on")
+        } else {
+            mqttClient.publish("rpi/gpio", withString: "off")
+        }
     }
 
 
+    @IBAction func connectButton(_ sender: UIButton) {
+        sender.setTitleColor(UIColor.red, for: .normal)
+        self.disconnBtn.setTitleColor(UIColor.darkText, for: .normal)
+        _ = mqttClient.connect()
+    }
+
+    
+    @IBAction func diconnectButton(_ sender: UIButton) {
+        sender.setTitleColor(UIColor.red, for: .normal)
+        self.connBtn.setTitleColor(UIColor.darkText, for: .normal)
+        mqttClient.disconnect()
+    }
 }
 
